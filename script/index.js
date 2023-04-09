163c89dcc37dfa8fc52ba5bcbcd73c85210df77e
MirkhakimAzizov <azizov.mirkhakimjon@gmail.com>
2023-04-09 12:54:10 +0500

    register page end

--- script/index.js
+++ script/index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let elForm = document.querySelector('.register__inner');
+
+elForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+    let value = evt.target.username.value.trim();
+    if (value != "" && value != " ") {
+        if (!localStorage.getItem('username')) {
+            localStorage.setItem('username', value);
+            window.location.href = './index.html';
+        } else {
+            window.location.href = './index.html';
+        }
+    }
+});
